39ea5363fbbc9250c010a0a2f72a8316b3a41515
satya <satya@DESKTOP-E78ITEC>
2025-04-02 09:43:05 +0530

    commit2

---
+++ homepractise/src/homepractise/Car.java
@@ -0,0 +1,35 @@
+package homepractise;
+
+public class Car {
+	  
+	    private String brand;
+	    private String model;
+	    private int year;
+
+	   
+	    public Car(String brand, String model, int year) {
+	        this.brand = brand;
+	        this.model = model;
+	        this.year = year;
+	    }
+
+	    
+	    public void displayDetails() {
+	        System.out.println("Brand: " + brand);
+	        System.out.println("Model: " + model);
+	        System.out.println("Year: " + year);
+	    }
+
+	    public static void main(String[] args) {
+	        
+	        Car myCar = new Car("Toyota", "Corolla", 2020);
+	        
+	        myCar.displayDetails();
+	    }
+	
+												
+
+
+	}
+
+
